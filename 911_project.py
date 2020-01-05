8026864868865a028f960c73c09c3784295d7e75
Jan Macioszczyk <janmac89@gmail.com>
2020-01-05 10:08:23 +0100

    Wyswietla informacje o pakiecie danych i najpopularniejsze kody pocztowe

--- 911_project.py
+++ 911_project.py
@@ -4,3 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv("911.csv")
+
+print("Informacje o pobranych danych: \n", df.info())
+print("Nagłówek pobranych danych: \n", df.head())
+print("Najpopularniejsze kody pocztowe: \n", df['zip'].value_counts().head(5))
